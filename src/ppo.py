911ac50e1a3379f88f7874051bc46e4409417daa
jakelevi1996 <jakelevi@hotmail.co.uk>
2023-05-26 00:53:04 +0100

    Add GruAC class for recurrent actor-critic

--- src/ppo.py
+++ src/ppo.py
@@ -107,6 +107,80 @@ class ActorCritic(nn.Module):
 
         return action_logprobs, torch.squeeze(state_value), dist_entropy
 
+class GruAC(nn.Module):
+    def __init__(self, state_dim, action_dim):
+        super(GruAC, self).__init__()
+        # self.hidden_dim = 256
+        self.hidden_dim = 200
+        self.actor_gru = nn.GRU(
+            input_size=state_dim,
+            hidden_size=self.hidden_dim,
+            num_layers=1,
+            batch_first=False,
+        )
+        self.actor_linear = nn.Linear(self.hidden_dim, action_dim * 2)
+        self.actor_state = None
+
+        # self.critic_gru = nn.GRU(
+        #     input_size=state_dim,
+        #     hidden_size=self.hidden_dim,
+        #     num_layers=1,
+        #     batch_first=False,
+        # )
+        self.critic_linear = nn.Linear(self.hidden_dim, 1)
+
+        self.action_dim = action_dim
+        self.state_dim = state_dim
+
+    def forward(self):
+        raise NotImplementedError
+
+    def act(self, state, memory):
+        batch_size, state_dim = state.shape
+        # assert state_dim == self.state_dim
+        state = state.reshape(1, batch_size, state_dim)
+
+        if self.actor_state is None:
+            h, self.actor_state = self.actor_gru(state)
+        else:
+            h, self.actor_state = self.actor_gru(state, self.actor_state)
+
+        h = h.reshape(batch_size, self.hidden_dim)
+        action_mean, action_var = torch.split(self.actor_linear(h), self.action_dim, dim=-1)
+        cov_mat = torch.diag_embed(F.softplus(action_var))
+
+        dist = MultivariateNormal(action_mean, cov_mat)
+
+        action = dist.sample()
+        action_logprob = dist.log_prob(action)
+
+        memory.states.append(state.reshape(batch_size, state_dim))
+        memory.actions.append(action)
+        memory.logprobs.append(action_logprob)
+
+        return action.detach()
+
+    def evaluate(self, state, action):
+        seq_len, batch_size, state_dim = state.shape
+        assert state_dim == self.state_dim
+        h, _ = self.actor_gru(state)
+        action_mean, action_var = torch.split(self.actor_linear(h), self.action_dim, dim=-1)
+        action_var = F.softplus(action_var)
+
+        #         action_var = self.action_var.expand_as(action_mean)
+        cov_mat = torch.diag_embed(action_var).to(device)
+
+        dist = MultivariateNormal(action_mean, cov_mat)
+
+        action_logprobs = dist.log_prob(action)
+        dist_entropy = dist.entropy()
+
+        # h, _ = self.critic_gru(state)
+        state_value = self.critic_linear(h)
+
+        self.actor_state = None
+        return action_logprobs, torch.squeeze(state_value), dist_entropy
+
 
 class PPO:
     def __init__(self, state_dim, action_dim, lr, betas, gamma, K_epochs, eps_clip, entropy):
